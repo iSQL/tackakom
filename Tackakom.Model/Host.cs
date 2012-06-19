ad6bc153a7546644bfbb39d4bf4805462b51f41e
maremilojkovic <maremilojkovic@e02bb6ef-be44-ff44-9cf3-76330ac0348f>
2012-06-19 13:25:52 +0000

    Dodati modeli

---
+++ Tackakom.Model/Host.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tackakom.Model
+{
+    public class Host
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Description { get; set; }
+        public int Image { get; set; }
+        public int Adress { get; set; }
+    }
+}

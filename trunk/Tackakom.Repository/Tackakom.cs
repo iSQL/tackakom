cbf7478bc9dc2f536b9e724888daa173cf5507c1
maremilojkovic <maremilojkovic@e02bb6ef-be44-ff44-9cf3-76330ac0348f>
2012-06-19 13:25:52 +0000

    Dodati modeli

---
+++ trunk/Tackakom.Repository/Tackakom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using Tackakom.Model;
+
+namespace Tackakom.Repository
+{
+     public class Tackakom : DbContext
+    {
+         public DbSet<Host> Hosts { get; set; }
+         public DbSet<Event> Events { get; set; }
+         public DbSet<EventCategory> EventCategories { get; set; }
+         public DbSet<Icon> Icons { get; set; } 
+    }
+}

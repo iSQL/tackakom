efb7dadb4248f74eb09b8c4f86f3b94f994c5104
maremilojkovic@gmail.com <maremilojkovic@gmail.com@e02bb6ef-be44-ff44-9cf3-76330ac0348f>
2012-06-20 20:22:43 +0000

    Icon Controllers/IconController.cs

---
+++ trunk/Tackakom.Repository/DbTackakom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using Tackakom.Model;
+
+namespace Tackakom.Repository
+{
+    public class DbTackakom : DbContext
+    {
+        public DbSet<Host> Hosts { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> EventCategories { get; set; }
+        public DbSet<Icon> Icons { get; set; }
+
+        
+    }
+}

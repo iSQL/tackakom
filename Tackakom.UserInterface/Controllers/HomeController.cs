65ca992826085d71f98afe60ffa36ba686c788de
maremilojkovic <maremilojkovic@e02bb6ef-be44-ff44-9cf3-76330ac0348f>
2012-08-09 12:41:41 +0000

    nesto

--- Tackakom.UserInterface/Controllers/HomeController.cs
+++ Tackakom.UserInterface/Controllers/HomeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Tackakom.UserInterface.Controllers
 {
     public class HomeController : Controller
     {
+
+        
         public ActionResult Index()
         {
+           // System.Web.HttpContext.Current.User.Identity.
+           // Guid guid = (Guid)Membership.GetUser().ProviderUserKey;
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
